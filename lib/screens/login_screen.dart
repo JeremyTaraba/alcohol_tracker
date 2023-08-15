ebb0175df29c79802fd597a097c511d16162de36
Jeremy Taraba <jeremytaraba@gmail.com>
2023-08-14 19:39:31 -0700

    Change spinner to modal progress indicator

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,7 +3,8 @@ import 'package:alcohol_tracker/screens/signup_screen.dart';
 import 'package:alcohol_tracker/util/buttons.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../util/constants.dart';
 
@@ -26,102 +27,102 @@ class _LoginScreenState extends State<LoginScreen> {
     return Container(
       color: Colors.lightBlue,
       child: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          body: Column(
-            children: [
-              showSpinner
-                  ? SpinKitWaveSpinner(color: Colors.blue)
-                  : Container(),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 12.0, right: 12.0, bottom: 25, top: 50),
-                child: TextField(
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  style: kTextFieldFont(),
-                  autocorrect: false,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration:
-                      kTextFieldDecoration().copyWith(hintText: "Email"),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 12.0, right: 12.0, bottom: 25.0),
-                child: TextField(
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  style: kTextFieldFont(),
-                  autocorrect: false,
-                  obscureText: true,
-                  keyboardType: TextInputType.text,
-                  decoration:
-                      kTextFieldDecoration().copyWith(hintText: "Password"),
-                ),
-              ),
-              LoginButton(onPressed: () async {
-                setState(() {
-                  showSpinner = true;
-                });
-
-                try {
-                  final user = await _auth.signInWithEmailAndPassword(
-                      email: email, password: password);
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => HomeScreen()));
-                } catch (e) {
-                  print(e);
-                }
-                setState(() {
-                  showSpinner = false;
-                });
-              }),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: const Text(
-                    "Forgot password?",
-                    style: TextStyle(color: Colors.blue, fontSize: 20),
+        child: ModalProgressHUD(
+          inAsyncCall: showSpinner,
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            body: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 12.0, right: 12.0, bottom: 25, top: 50),
+                  child: TextField(
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    style: kTextFieldFont(),
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration:
+                        kTextFieldDecoration().copyWith(hintText: "Email"),
                   ),
                 ),
-              ),
-              Expanded(
-                child: Align(
-                  alignment: AlignmentDirectional.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          "Don't have an account? ",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SignUpScreen()));
-                          },
-                          child: const Text(
-                            "Sign up",
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 20,
-                            ),
-                          ),
-                        ),
-                      ],
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 12.0, right: 12.0, bottom: 25.0),
+                  child: TextField(
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    style: kTextFieldFont(),
+                    autocorrect: false,
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                    decoration:
+                        kTextFieldDecoration().copyWith(hintText: "Password"),
+                  ),
+                ),
+                LoginButton(onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+
+                  try {
+                    final user = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                  } catch (e) {
+                    print(e);
+                  }
+                  setState(() {
+                    showSpinner = false;
+                  });
+                }),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: const Text(
+                      "Forgot password?",
+                      style: TextStyle(color: Colors.blue, fontSize: 20),
                     ),
                   ),
                 ),
-              )
-            ],
+                Expanded(
+                  child: Align(
+                    alignment: AlignmentDirectional.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 15.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Don't have an account? ",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SignUpScreen()));
+                            },
+                            child: const Text(
+                              "Sign up",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
