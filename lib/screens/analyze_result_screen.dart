c87a960af3644e011c634cb7b81fd613edf94757
Jeremy Taraba <jeremytaraba@gmail.com>
2023-08-19 21:27:43 -0700

    Added results screen

---
+++ lib/screens/analyze_result_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AnalyzeResultsScreen extends StatelessWidget {
+  const AnalyzeResultsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("Pick the appropriate result"),
+      ),
+    );
+  }
+}
