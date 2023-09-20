dea2461f398b52f06c6c500ca244e3faf6d7dd25
Jeremy Taraba <jeremytaraba@gmail.com>
2023-09-20 16:00:00 -0700

    Added bottom nav for history screen

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,18 +1,46 @@
+import 'package:alcohol_tracker/util/bottom_nav.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-import '../util/bottom_nav.dart';
-
-class HistoryScreen extends StatelessWidget {
+class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
 
   @override
+  State<HistoryScreen> createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Text("History of drinks here"),
-      ),
-      bottomNavigationBar: bottomNav(
-        selectedIndex: 2,
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          body: Container(
+            child: TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: _focusedDay,
+              availableCalendarFormats: const {CalendarFormat.month: "Month"},
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              pageJumpingEnabled: true,
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay; // update `_focusedDay` here as well
+                });
+              },
+            ),
+          ),
+          bottomNavigationBar: bottomNav(
+            selectedIndex: 2,
+          ),
+        ),
       ),
     );
   }
